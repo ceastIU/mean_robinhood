078faffa78a62de866929f265a6f21c287e7254d
ceastIU <ceast@umail.iu.edu>
2017-09-29 17:53:22 -0400

    Hi

--- app.js
+++ app.js
@@ -8,26 +8,23 @@ const PORT = process.env.PORT || 3001;
 var robin = require('./robin');
 
 hbs.registerPartials(__dirname + '/views/partials');
+
+hbs.registerHelper('times', function(stock, block) {
+    var accum = '';
+    for(var i = 0; i < stock.length; ++i)
+        accum += block.fn(stock[i]);
+    return accum;
+});
+
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) =>{
     res.render('index.hbs',{
         owner: "Christopher East",
+        default: "TSLA",
+        stocks: ["TSLA","HMNY"]
     });
-    robin.get(req.params.id).then((data)=>{
-        res.render('stock.hbs',{
-            stock: stockId,
-            price: '$' + data
-        });
-    }).catch((data) => {
-        console.log(data);
-        res.render('stock.hbs',{
-            stock: stockId,
-            price: data
-        });
-    });
-     
 });
 
 
